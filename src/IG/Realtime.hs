9fb39963a479bd39970e26fd55e4dfe824bc559d
James Hamilton <wjdhamilton@balnaandevelopments.co.uk>
2017-04-26 10:31:25 +0100

    Improved visibility in Realtime module

--- src/IG/Realtime.hs
+++ src/IG/Realtime.hs
@@ -19,7 +19,7 @@
 -- id and session url using a control connection. The control connection is run
 -- from a different thread than that which consumes the subscription. 
 
-module IG.Realtime  where
+module IG.Realtime (openSession, control, RealTimeError(..)) where
 
 import Control.Concurrent
 import Control.Concurrent.STM as STM
